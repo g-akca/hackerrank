b4acd4d1cfb3af0b4ec2cb1a6a69ac7674c977ef
g-akca <gok.akca66@gmail.com>
2025-04-19 00:19:10 +0300

    Algorithms - Solve Me First

---
+++ Algorithms/Warmup/Solve Me First/Solution.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << solveMeFirst(a, b);
+    return 0;
+}
