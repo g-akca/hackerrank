f87596195793cc7bbcee47c7068a9ecf61218f6b
g-akca <gok.akca66@gmail.com>
2025-04-19 11:56:36 +0300

    C++ - Basic Data Types

---
+++ C++/Introduction/Basic Data Types/Solution.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a;
+    long int b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >> e;
+    printf("%i\n%li\n%c\n%.3f\n%.9lf", a, b, c, d, e);
+    return 0;
+}
