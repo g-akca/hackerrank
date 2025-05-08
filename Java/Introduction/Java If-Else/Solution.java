13888f63e3920d4ab1ad3cf310e86cb055f96339
g-akca <gok.akca66@gmail.com>
2025-05-08 22:18:36 +0300

    Java - Java If-Else

---
+++ Java/Introduction/Java If-Else/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        
+        scanner.close();
+        if (N % 2 == 1) {
+            System.out.println("Weird");
+        }
+        else if (2 <= N && N <= 5) {
+            System.out.println("Not Weird");
+        }
+        else if (6 <= N && N <= 20) {
+            System.out.println("Weird");
+        }
+        else if (20 <= N) {
+            System.out.println("Not Weird");
+        }
+    }
+}
