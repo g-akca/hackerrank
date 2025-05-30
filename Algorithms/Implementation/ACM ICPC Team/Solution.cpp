b66613c9810f55c5a46d0ad85aff0c4d9e7b5b59
g-akca <gok.akca66@gmail.com>
2025-05-31 02:14:14 +0300

    Algorithms - ACM ICPC Team

---
+++ Algorithms/Implementation/ACM ICPC Team/Solution.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'acmTeam' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts STRING_ARRAY topic as parameter.
+ */
+
+vector<int> acmTeam(vector<string> topic) {
+    vector<int> max(2, 0);
+    for (int i = 0; i < topic.size() - 1; i++) {
+        for (int j = i + 1; j < topic.size(); j++) {
+            int total_subjects = 0;
+            for (int k = 0; k < topic[0].size(); k++) {
+                if (topic[i][k] == '1' || topic[j][k] == '1') {
+                    total_subjects++;
+                }
+            }
+            
+            if (total_subjects > max[0]) {
+                max[0] = total_subjects;
+                max[1] = 1;
+            }
+            else if (total_subjects == max[0]) {
+                max[1]++;
+            }
+        }
+    }
+    
+    return max;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int m = stoi(first_multiple_input[1]);
+
+    vector<string> topic(n);
+
+    for (int i = 0; i < n; i++) {
+        string topic_item;
+        getline(cin, topic_item);
+
+        topic[i] = topic_item;
+    }
+
+    vector<int> result = acmTeam(topic);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
