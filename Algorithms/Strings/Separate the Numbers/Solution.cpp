49a3dbde63a037403b202c1cfde155b09a6d7e48
g-akca <gok.akca66@gmail.com>
2025-06-22 22:34:21 +0300

    Algorithms - Separate the Numbers

---
+++ Algorithms/Strings/Separate the Numbers/Solution.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'separateNumbers' function below.
+ *
+ * The function accepts STRING s as parameter.
+ */
+
+void separateNumbers(string s) {
+    for (int len = 1; len <= s.length() / 2; len++) {
+        string first = s.substr(0, len);
+        if (first[0] == '0') break;
+        long long num = stoll(first);
+        string temp = first;
+
+        while (temp.length() < s.length()) {
+            num++;
+            temp += to_string(num);
+        }
+
+        if (temp == s) {
+            cout << "YES " << first << endl;
+            return;
+        }
+    }
+    cout << "NO" << endl;
+}
+
+int main()
+{
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        separateNumbers(s);
+    }
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
