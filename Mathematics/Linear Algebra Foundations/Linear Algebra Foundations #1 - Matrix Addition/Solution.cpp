81df075fb011e5b4bd6c15d99bd431b268f2946d
g-akca <gok.akca66@gmail.com>
2025-11-04 15:48:28 +0300

    Mathematics -  Linear Algebra Foundations #1 - Matrix Addition

---
+++ Mathematics/Linear Algebra Foundations/Linear Algebra Foundations #1 - Matrix Addition/Solution.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    vector<int> res = {5, 7, 9, 9, 11, 13, 5, 6, 8};
+    for (auto& a : res) {
+        cout << a << endl;
+    }
+    return 0;
+}
