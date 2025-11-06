dee175ccbdda297ca7083d133ce123f7a215b372
g-akca <gok.akca66@gmail.com>
2025-11-06 11:21:26 +0300

    Mathematics -  Linear Algebra Foundations #2 - Matrix Subtraction

---
+++ Mathematics/Linear Algebra Foundations/Linear Algebra Foundations #2 - Matrix Subtraction/Solution.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    vector<int> res = {-3, -3, -3, -5, -5, -5, -3, -4, 1};
+    for (auto& a : res) {
+        cout << a << endl;
+    }  
+    return 0;
+}
