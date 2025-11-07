9f2a338eab5179ab5fe6bef7f0c66e2ab884aa29
g-akca <gok.akca66@gmail.com>
2025-11-07 21:12:02 +0300

    Mathematics -  Linear Algebra Foundations #3- Matrix Multiplication

---
+++ Mathematics/Linear Algebra Foundations/Linear Algebra Foundations #3- Matrix Multiplication/Solution.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    vector<int> res = {18, 21, 29, 34};
+    for (auto& a : res) {
+        cout << a << endl;
+    }
+    return 0;
+}
